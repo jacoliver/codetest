ca59de4b6d7d2ab59286232ffe26412871426260
Jac Domney <jac@campaignagent.com.au>
2020-10-03 08:11:50 +1000

    Initial Commit

---
+++ CodeTest/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeTest
+{
+    //Shapes Inc. has requested your services to build an application that automates calculations that they currently perform manually
+    //They have provided you with a csv file (available in this solution)
+    //The first column in the csv file denotes the shape, "C" for Circle, "R" for rectangle
+    //The second column represents either the radius of a circle or the length of the rectangle (depending on the value of the first column)
+    //The 3rd column will not be filled for a circle, but will represent the height of a rectangle
+
+    //Shapes Inc. would like an application that prints information regarding the 5 largest shapes by area
+    //They would also like the aplication to print 5 smallest shapes by perimeter
+
+    //**BONUS QUESTION**
+    //Shapes Inc. wish to continue the development of the application after you have built the initial program
+    //As such they have asked if you could write unit tests where appropriate to ensure quality
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
